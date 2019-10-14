c2d34206331db16b26e529b53ba61872f8829612
Hans Schouten <hans@falcosolutions.nl>
2019-10-14 19:35:50 +0200

    Pagebuilder config added

---
+++ config/pagebuilder.example.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Project settings
+     |--------------------------------------------------------------------------
+     |
+     | Settings related to the project in which the PageBuilder is included.
+     |
+     */
+    'project' => [
+        'public_folder' => '',
+    ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Themes settings
+     |--------------------------------------------------------------------------
+     |
+     | PageBuilder requires a themes folder in which for each theme the individual
+     | theme blocks are defined.
+     |
+     */
+    'themes' => [
+        'folder' => '',
+        'active_theme' => ''
+    ],
+];
--- src/PHPageBuilder.php
+++ src/PHPageBuilder.php
@@ -5,10 +5,34 @@ namespace PHPageBuilder;
 class PHPageBuilder
 {
     /**
+     * @var array $config
+     */
+    protected $config;
+
+    /**
+     * PHPageBuilder constructor.
+     *
+     * @param array $config   configuration in the format defined in config/pagebuilder.example.php
+     */
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Return all blocks of the current theme.
+     */
+    public function getThemeBlocks()
+    {
+    }
+
+    /**
      * Render the pagebuilder.
      */
     public function render()
     {
+        // render the pagebuilder view while passing this class instance as $pagebuilder
+        $pagebuilder = $this;
         require_once 'resources/views/pagebuilder.php';
     }
 }
