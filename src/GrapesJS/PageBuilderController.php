a0f2a08644bd8bee4b7775fb3f0ff992b2fcabf4
Hans Schouten <hans@falcosolutions.nl>
2019-10-16 23:53:24 +0200

    GrapesJS files moved to separate folder

---
+++ src/GrapesJS/PageBuilderController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PHPageBuilder\GrapesJS;
+
+class PageBuilderController
+{
+    /**
+     * PageBuilderController constructor.
+     */
+    public function __construct()
+    {
+    }
+}
