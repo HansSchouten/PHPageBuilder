6dc96971ac942893f82687596a2227ae8da3b20c
Hans Schouten <hans@falcosolutions.nl>
2019-12-09 14:31:29 +0100

    Block filtering functionality added

--- src/Modules/GrapesJS/resources/assets/js/app.js
+++ src/Modules/GrapesJS/resources/assets/js/app.js
@@ -1,4 +1,5 @@
 import './pagebuilder';
+import './block-search';
 import './save-page';
 import './manage-editable-components';
 import './ckeditor-hyperlinks';
---
+++ src/Modules/GrapesJS/resources/assets/js/block-search.js
@@ -0,0 +1,13 @@
+
+$(document).on("input", ".block-search input", function() {
+    let term = $(this).val().toLowerCase();
+
+    $(".gjs-blocks-cs .gjs-block").each(function() {
+        let label = $(this).text().toLowerCase();
+        if (label.includes(term)) {
+            $(this).removeClass('d-none');
+        } else {
+            $(this).addClass('d-none');
+        }
+    });
+});
