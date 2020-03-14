cb88fb6eaff0acfead0dc69cd958f6f4383d02fe
Hans Schouten <hans@falcosolutions.nl>
2020-03-14 17:10:45 +0100

    Link type settings removed

--- src/Modules/GrapesJS/resources/views/grapesjs/component-type-manager.php
+++ src/Modules/GrapesJS/resources/views/grapesjs/component-type-manager.php
@@ -1,34 +1,12 @@
 <script type="text/javascript">
 
-let linkType = editor.DomComponents.getType('link');
 editor.DomComponents.addType('link', {
-    model: linkType.model.extend({
-        defaults: Object.assign({}, linkType.model.prototype.defaults, {
-            traits: [
-                {
-                    type: 'text',
-                    label: 'URL',
-                    name: 'href',
-                },
-                {
-                    type: 'select',
-                    label: '<?= phpb_trans('pagebuilder.trait-manager.link.target') ?>',
-                    name: 'target',
-                    options: [
-                        {value: '_blank', name: '<?= phpb_trans('pagebuilder.yes') ?>'},
-                        {value: '', name: '<?= phpb_trans('pagebuilder.no') ?>'},
-                    ]
-                }
-            ],
-        }),
-    }, {
-        isComponent: function(el) {
-            if (el.tagName === 'A') {
-                return {type: 'link'};
-            }
+    model: {
+        defaults: {
+            traits: [],
+            attributes: {},
         },
-    }),
-    view: linkType.view,
+    },
 });
 
 editor.DomComponents.addType('text', {
