2cd96b85856b2a2e56c3ecd369bed096d08df774
Hans Schouten <hans@falcosolutions.nl>
2020-12-01 20:59:33 +0100

    Hide block categories without any matches

--- src/Modules/GrapesJS/resources/assets/js/block-search.js
+++ src/Modules/GrapesJS/resources/assets/js/block-search.js
@@ -2,16 +2,26 @@
 $(document).on("input", "#block-search input", function() {
     let term = $(this).val().toLowerCase();
 
-    $(".gjs-blocks-cs .gjs-block").each(function() {
-        let label = $(this).text().toLowerCase();
-        if (label.includes(term)) {
-            $(this).removeClass("d-none");
+    $(".gjs-block-category").each(function() {
+        let atLeastOneMatch = false;
 
-            let regEx = new RegExp('(' + term + ')', "gi");
-            $(this).find(".gjs-block-label").html(
-                $(this).text().replace(regEx, '<b>$1</b>')
-            );
-        } else {
+        $(this).find(".gjs-block").each(function() {
+            let label = $(this).text().toLowerCase();
+            if (label.includes(term)) {
+                $(this).removeClass("d-none");
+                atLeastOneMatch = true;
+
+                let regEx = new RegExp('(' + term + ')', "gi");
+                $(this).find(".gjs-block-label").html(
+                    $(this).text().replace(regEx, '<b>$1</b>')
+                );
+            } else {
+                $(this).addClass("d-none");
+            }
+        });
+
+        $(this).removeClass("d-none");
+        if (! atLeastOneMatch) {
             $(this).addClass("d-none");
         }
     });
