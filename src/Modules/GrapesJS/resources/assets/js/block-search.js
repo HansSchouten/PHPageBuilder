572f4eff9fbe798ef94c5be29afd853dfa68097c
Hans Schouten <hans@falcosolutions.nl>
2020-12-01 20:41:19 +0100

    Highlight sidebar search term matches

--- src/Modules/GrapesJS/resources/assets/js/block-search.js
+++ src/Modules/GrapesJS/resources/assets/js/block-search.js
@@ -6,6 +6,9 @@ $(document).on("input", "#block-search input", function() {
         let label = $(this).text().toLowerCase();
         if (label.includes(term)) {
             $(this).removeClass("d-none");
+            $(this).find(".gjs-block-label").html(
+                $(this).text().replace(term, '<b>' + term + '</b>')
+            );
         } else {
             $(this).addClass("d-none");
         }
