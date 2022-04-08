d05a43912dbfea95a1d9dde00424f43b1316a305
Hans Schouten <hans@falcosolutions.nl>
2022-04-08 10:59:56 +0200

    Fix setLanguage, check whether keys are valid languages (renderPageBuilderBlock uses pageData without language data)

--- src/Modules/GrapesJS/PageRenderer.php
+++ src/Modules/GrapesJS/PageRenderer.php
@@ -81,10 +81,22 @@ class PageRenderer
      */
     public function setLanguage($language)
     {
+        // if the given language is unknown, default the set language to the first available language
+        $blockKeysAreLanguages = true;
         $supportedLanguages = array_keys($this->pageData['blocks'] ?? []);
-        if (! in_array($language, $supportedLanguages)) {
-            $language = $supportedLanguages[0] ?? $language;
+        // check whether keys are valid languages (renderPageBuilderBlock uses pageData without language data)
+        foreach ($supportedLanguages as $supportedLanguage) {
+            if (strlen($supportedLanguage) > 5) {
+                $blockKeysAreLanguages = false;
+                break;
+            }
         }
+        if ($blockKeysAreLanguages) {
+            if (! in_array($language, $supportedLanguages)) {
+                $language = $supportedLanguages[0] ?? $language;
+            }
+        }
+
         $this->language = $language;
         $this->pageBlocksData = $this->getStoredPageBlocksData();
         $this->shortcodeParser->setLanguage($language);
