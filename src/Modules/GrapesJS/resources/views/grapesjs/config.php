1da4f803de795c16209a0d96b8ca416d4e340735
Hans Schouten <hans@falcosolutions.nl>
2023-04-26 14:17:47 +0200

    Force paste as plaintext, improve editor toolbar

--- src/Modules/GrapesJS/resources/views/grapesjs/config.php
+++ src/Modules/GrapesJS/resources/views/grapesjs/config.php
@@ -96,28 +96,17 @@ return [
             'options' => [
                 'startupFocus' => true,
                 'allowedContent' => true,
+                'forcePasteAsPlainText' => true,
                 //'extraAllowedContent' => '*(*);*[*];ul ol li span', // allows classes, inline styles and certain elements
                 'enterMode' => 'CKEDITOR.ENTER_BR',
-                'extraPlugins' => 'sharedspace,justify,colorbutton,font',
+                'extraPlugins' => 'sourcedialog',
                 'toolbar' => [
                     ['Bold', 'Italic', 'Underline', 'Strike', 'Undo', 'Redo'],
-                    [
-                        'name' => 'links',
-                        'items' => ['Link', 'Unlink']
-                    ],
-                    [
-                        'name' => 'styles',
-                        'items' => ['FontSize']
-                    ],
-                    [
-                        'name' => 'paragraph',
-                        'groups' => ['list', 'indent', 'blocks', 'align'],
-                        'items' => ['NumberedList', 'BulletedList', 'Blockquote', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-']
-                    ],
-                    [
-                        'name' => 'colors',
-                        'items' => ['TextColor', 'BGColor']
-                    ],
+                    ['Link', 'Unlink'],
+                    ['NumberedList', 'BulletedList'],
+                    ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+                    ['FontSize', 'TextColor'],
+                    ['Sourcedialog']
                 ],
             ]
         ]
