2b09b49f882120e443b40190249226aced111e3d
Hans Schouten <hans@falcosolutions.nl>
2023-08-07 12:51:18 +0200

    Prepend pagebuilder style for less layout shifting

--- src/Modules/GrapesJS/PageRenderer.php
+++ src/Modules/GrapesJS/PageRenderer.php
@@ -236,7 +236,7 @@ class PageRenderer
 
         // include any style changes made via the page builder
         if (isset($data['css'])) {
-            $html .= '<style>' . $data['css'] . '</style>';
+            $html = '<style>' . $data['css'] . '</style>' . $html;
         }
 
         return $html;
