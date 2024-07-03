e81f26cfdde63ae9f83ede2f755934a343e37a99
Hans Schouten <hans@falcosolutions.nl>
2024-07-03 19:54:15 +0200

    Update to ckeditor v4.21.0 full

--- src/Modules/GrapesJS/resources/views/pagebuilder.php
+++ src/Modules/GrapesJS/resources/views/pagebuilder.php
@@ -15,7 +15,8 @@
 
 <div id="gjs"></div>
 
-<script type="text/javascript" src="<?= phpb_asset('pagebuilder/ckeditor/ckeditor.js') ?>"></script> <?php // local copy of 4.21.0 (full-all) ?>
+<!--<script type="text/javascript" src="https://cdn.ckeditor.com/4.21.0/full-all/ckeditor.js"></script>-->
+<script type="text/javascript" src="<?= phpb_asset('pagebuilder/ckeditor/ckeditor.js') ?>"></script>
 <script type="text/javascript" src="<?= phpb_asset('pagebuilder/grapesjs-plugin-ckeditor-v0.0.10.min.js') ?>"></script>
 <script type="text/javascript" src="<?= phpb_asset('pagebuilder/grapesjs-touch-v0.1.1.min.js') ?>"></script>
 <script type="text/javascript">
