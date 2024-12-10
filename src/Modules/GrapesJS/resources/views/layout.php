74f77cca7f36b3b2b2491e58742428db553f863c
Hans Schouten <hans@falcosolutions.nl>
2024-12-10 12:58:26 +0100

    Prevent loading favicon.ico which does not exist

--- src/Modules/GrapesJS/resources/views/layout.php
+++ src/Modules/GrapesJS/resources/views/layout.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?= phpb_asset('pagebuilder/toastr-v2.1.3.min.css') ?>">
     <link rel="stylesheet" href="<?= phpb_asset('pagebuilder/app.css') ?>">
+    <link rel="icon" href="data:,">
     <?= $pageBuilder->customStyle(); ?>
 
     <script src="<?= phpb_asset('pagebuilder/grapesjs-v0.15.9.min.js') ?>"></script>
